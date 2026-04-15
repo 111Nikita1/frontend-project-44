1008fa0eb38e8a893ea354a99c8f47424c45cde8
111Nikita1 <crazyshot411@gmail.com>
2026-04-15 23:25:03 +0300

    step8

---
+++ games/progression.js
@@ -0,0 +1,27 @@
+const description = 'What number is missing in the progression?';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateProgression = (start, step, length) => {
+  const progression = [];
+  for (let i = 0; i < length; i += 1) {
+    progression.push(start + i * step);
+  }
+  return progression;
+};
+
+const generateRound = () => {
+  const length = getRandomInt(5, 10);
+  const start = getRandomInt(1, 50);
+  const step = getRandomInt(1, 10);
+  const hiddenIndex = getRandomInt(0, length - 1);
+
+  const progression = generateProgression(start, step, length);
+  const correctAnswer = String(progression[hiddenIndex]);
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
+
+  return { question, correctAnswer };
+};
+
+export { description, generateRound };
